bd82fb4135dd66545e8889b30fc32ea2d5696ff8
veronica pupo <veronica.pupo@hotmail.com>
2022-09-29 17:26:37 -0300

    add dependencias

---
+++ src/main/java/com/dio/bag/BagApiApplication.java
@@ -0,0 +1,13 @@
+package com.dio.bag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BagApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BagApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio/bag/BagApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.bag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BagApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
